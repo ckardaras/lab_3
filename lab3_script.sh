e697832bd745a6c00df6239157c7b5e89ee3db98
ckardaras <ckardaras92@gmail.com>
2019-09-25 14:46:17 -0700

    Made some changes

--- lab3_script.sh
+++ lab3_script.sh
@@ -7,7 +7,7 @@ echo "Enter File name: "
 read filename
 echo "Enter regex command: "
 read search
-grep -c @ $filename
-grep -o ^303 $filename
-grep $search $filename >> email_results.txt
-
+grep -c $search $filename
+grep -c '[0-9]\{3\}-[0-9]\{3\}-[0-9]\{4\}' regex_practice.txt
+grep -o '303-[0-9]\{3\}-[0-9]\{4\}' regex_practice.txt
+grep @ regex_practice.txt >> email_results.txt
